12260923550cd4591a06014ddd87225484546d0c
JC Hong <jaechol.hong@outlook.com>
2025-03-04 22:13:21 +0100

    comments section

---
+++ src/components/Comments.jsx
@@ -0,0 +1,13 @@
+const Comments = () => {
+  return (
+    <div className="flex flex-col gap-8 lg:w-3/5">
+      <h1 className="text-xl text-gray-500 underline">Comments</h1>
+      <div className="flex items-center justify-between gap-8 w-full">
+        <textarea placeholder="write a comment..." />
+        <button className="bg-blue-800 px-4 py-3 text-white-medium rounded-xl">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,7 +1,84 @@
+import { Link } from "react-router-dom";
+import Image from "../components/Image";
+import PostMenuActions from "../components/PostMenuActions";
+import Search from "../components/Search";
+import Comments from "../components/Comments";
+
 const SinglePostPage = () => {
   return (
-    <div className="">SinglePostPage</div>
-  )
+    <div className="flex flex-col gap-8">
+      {/* Detail */}
+      <div className="flex gap-8">
+        <div className="lg:-3/5 flex flex-col gap-8">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link className="text-blue-800">John Doe</Link>
+            <span>on</span>
+            <Link className="text-blue-800">Web Design</Link>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-gray-500 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim corporis ipsam voluptates maxime suscipit dolorum quia, aperiam quasi? Dolorem, aliquam? Dicta quia rerum pariatur itaque ad fuga aliquid ullam quidem.</p>
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg" w="600" className="rounded-2xl" />
+        </div>
+      </div>
+      {/* Content */}
+      <div className="flex flex-col md:flex-row gap-12">
+        {/* Text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel odio est. Morbi consequat, quam nec accumsan consectetur, tellus enim ullamcorper velit, quis pretium urna enim ut ligula. Phasellus tempor mi a dignissim dapibus. Proin condimentum ante mi, ut tristique arcu elementum eget. In hac habitasse platea dictumst. Vestibulum varius diam ac lacinia hendrerit. Donec enim arcu, vulputate sed est a, congue porttitor mi. Suspendisse ac nulla hendrerit risus consectetur scelerisque ut et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus..</p>
+        </div>
+        {/* Menu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1 className="mb-4 text-sm font-medium">Author</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+              <Image
+                src="userImg.jpeg"
+                className="w-12 h-12 rounded-full object-cover"
+                w="48"
+                h="48"
+              />
+              <Link className="text-blue-800">John Doe</Link>
+            </div>
+              <p className="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <div className="flex gap-2">
+                <Link>
+                  <Image src="facebook.svg" />
+                </Link>
+                <Link>
+                  <Image src="instagram.svg" />
+                </Link>
+              </div>
+          </div>
+          
+          <PostMenuActions />
+          <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline">All</Link>
+            <Link className="underline">Web Design</Link>
+            <Link className="underline">Development</Link>
+            <Link className="underline">Databases</Link>
+            <Link className="underline">Search Engines</Link>
+            <Link className="underline">Marketing</Link>
+          </div>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
+          <Search />
+        </div>
+      </div>
+      <Comments />
+    </div>
+  );
 };
 
 export default SinglePostPage;
