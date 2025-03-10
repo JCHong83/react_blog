d383aa4754d803a45d18e43b43238a63d2824a81
JC Hong <jaechol.hong@outlook.com>
2025-03-10 15:01:37 +0100

    write and post list page

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,19 @@
+import Image from "./Image";
+
 const Comment = () => {
   return (
-    <div>Comment</div>
+    <div className="p-4 bg-slate-50 rounded-xl mb-8">
+      <div className="flex items-center gap-4">
+        <Image src="userImg.jpeg" className="w-10 h-10 rounded-full object-cover" w="40" />
+        <span className="font-medium">John Doe</span>
+        <span className="text-sm text-gray-500">2 days ago</span>
+      </div>
+      <div className="mt-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt, praesentium ea dignissimos voluptatibus officia incidunt ipsum, sed molestiae fugit exercitationem repellendus molestias! Rerum, sit. Libero, ipsa animi. Repellat, et.
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,6 +1,19 @@
+import PostList from "../components/PostList";
+import SideMenu from "../components/SideMenu";
+
 const PostListPage = () => {
   return (
-    <div className="">PostListPage</div>
+    <div className="">
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <div className="flex gap-8">
+        <div>
+          <PostList />
+        </div>
+        <div>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
   )
 };
 
